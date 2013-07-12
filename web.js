4165fffc7ebc18b742a958714ed9c106fa31893c
Craig LaValle <craig.lavalle@gmail.com>
2013-07-12 05:02:21 +0000

    changed web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   text = fs.readFile('index.html');
-  response.send(text);
+  response.send(text.toString());
 });
 
 var port = process.env.PORT || 5000;
